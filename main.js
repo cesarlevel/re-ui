21a8fbd993d52ad5b242031fcdb2b458c79dcce8
Cesar <cesarlevel@imac-01.local>
2017-12-13 17:11:46 -0500

    Adding popover

--- main.js
+++ main.js
@@ -1,48 +1,25 @@
-var tabListItems = document.querySelectorAll('.tab-link');
-var tabList = document.querySelector('.tab').childNodes;
-var tabContent = document.querySelectorAll('.tab-content');
+//Tab system
+'use strict';
+let tabListItems = document.querySelectorAll('.tab-link'),
+    tabContent = document.querySelectorAll('.tab-content');
 
-
-//Hide all .tab-content except the first one
-
-
-function clearTabs(){
-    for(var i = 0; i < tabListItems.length; i++){
+let clearTabs = () => {
+    for(let i = 0; i < tabListItems.length; i++){
         tabListItems[i].className = 'tab-link';
-    }
-
-    for(var i = 0; i < tabContent.length; i++){
-        if(i !== 0){
-            tabContent[i].className = 'tab-content visually-hidden';
-        }
+        tabContent[i].className = 'tab-content';
     }
 }
 
-//Adds .is-active class to the first tab
-
-
-//console.log(tabList);
-
-for(var i = 0; i < tabList.length; i++){
-    if(tabList[i].className === "tab-item"){
-        for(var j = 0; j < tabList[i].childNodes.length; j++){
-            if(tabList[i].childNodes[j].localName === "a"){
-                tabList[i].childNodes[j].addEventListener("click", tabing, false);
-                //console.log(tabList[i].childNodes[j]);
-            }
-
-        }
-
-    }
-
-}
-
-function tabing(event){
+let tabs = (event) => {
     clearTabs();
-    var target = event.target;
-    target.classList.add('is-active');
-    console.log(target)
 
+    let target = event.target,
+        content = document.querySelector(target.getAttribute("href"));
+
+    target.classList.add('is-active');
+    content.classList.add('is-active');
 }
 
-//tabList.addEventListener("click", tabing, false);
+for(let i = 0; i < tabListItems.length; i++){
+    tabListItems[i].addEventListener("click", tabs, false);
+}
